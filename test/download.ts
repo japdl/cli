abc7965ff7522425a25f2bcfab60814caec37aed
Seysa <garion.g@hotmail.fr>
2021-04-28 03:24:29 +0200

    added tests

--- test/download.ts
+++ test/download.ts
@@ -69,3 +69,72 @@ describe("Downloading one-piece chapter 999", function () {
     }
   });
 });
+describe("Fetch manga stats tests", function () {
+    this.timeout(0);
+  it("Fetchs nanatsu-no-taizai volumes, name and chapters", function () {
+    const supposedResults = {
+      volumes: 41,
+      chapters: 347,
+      name: "nanatsu-no-taizai",
+    };
+    return new Promise((resolve, reject) => {
+      downloader.fetchStats("nanatsu-no-taizai").then((infos) => {
+        const supposedResultsString = JSON.stringify(supposedResults);
+        const infosString = JSON.stringify(infos);
+        if (supposedResultsString !== infosString) {
+          reject(
+            "Les résultats obtenus ne sont pas les résultats attendus. Attendus: " +
+              supposedResultsString +
+              "\nObtenus: " +
+              infosString
+          );
+        }
+        resolve(undefined);
+      });
+    });
+  });
+  it("Fetchs one-piece volume 97 chapters", function () {
+      const supposedResults = [
+        'https://www.japscan.se/lecture-en-ligne/one-piece/976/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/977/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/978/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/979/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/980/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/981/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/982/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/983/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/984/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/985/',
+        'https://www.japscan.se/lecture-en-ligne/one-piece/986/'
+      ];
+      return new Promise((resolve, reject) => {
+        const supposedResultsString = supposedResults.toString();
+        downloader.fetchVolumeChapters(97, "one-piece").then((chapters) => {
+            const chaptersToString = chapters.toString();
+            if(chaptersToString !== supposedResultsString){
+              reject(
+                  "Les résultats obtenus ne sont pas les résultats attendus. Attendus: " +
+                    supposedResultsString +
+                    "\nObtenus: " +
+                    chaptersToString
+                );
+            }
+            resolve(undefined);
+        })
+      })
+  });
+});
+describe("japscan 404 tests", function(){
+    it("Should throw because page is 404", function() {
+        return new Promise((resolve, reject) => {
+            downloader.goToExistingPage("https://www.japscan.se/manga/one-piece").catch((error) => reject(error));
+            resolve(undefined);
+        })
+    });
+    it("Should not throw because page exists", function() {
+        return new Promise((resolve, reject) => {
+            downloader.goToExistingPage("https://www.japscan.se/manga/one-piece/").catch((error) => reject(error));
+            resolve(undefined);
+        })
+    })
+})
